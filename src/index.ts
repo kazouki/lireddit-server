a8a4db605e9b07edd3aa20347d4e6ffc43730a56
kazouki <kazouki@hotmail.com>
2020-08-31 21:45:55 +0200

    setup mikroORM index and dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
+import { MikroORM } from "@mikro-orm/core";
+import { __prod__ } from "./constants";
 
+require("dotenv").config();
+
+const main = async () => {
+  const orm = await MikroORM.init({
+    entities: [],
+    dbName: "lireddit",
+    type: "postgresql",
+    clientUrl: process.env.DATABASE_URL_DEV,
+    debug: !__prod__,
+  });
+};
+
+main();
